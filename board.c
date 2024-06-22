127446e348dcb2c73d0d1665c8ca0efb00cbf946
brunobahri <brunobahri27@gmail.com>
2024-06-22 18:27:13 -0300

    feat: board.c

---
+++ board.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "board.h"
+#include "tetromino.h"
+
+void drawBoard() {
+    system("clear"); // ou "cls" no Windows
+    int display[HEIGHT][WIDTH];
+    memcpy(display, board, sizeof(board));
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (current.shape[i][j]) {
+                display[current.y + i][current.x + j] = current.shape[i][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            if (display[i][j])
+                printf("#");
+            else
+                printf(".");
+        }
+        printf("\n");
+    }
+}
