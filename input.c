bf1427fad0fb9a054ede4ebd58a9f9cd75ca360a
brunobahri <brunobahri27@gmail.com>
2024-06-23 18:21:01 -0300

    feat: input.c

---
+++ input.c
@@ -0,0 +1,37 @@
+#include <sys/select.h>
+#include <unistd.h>
+#include "input.h"
+#include "tetromino.h"
+
+extern int fastDrop; // Declaração externa para acessar a variável global
+
+int kbhit() {
+    struct timeval tv = { 0L, 0L };
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds);
+    return select(STDIN_FILENO + 1, &fds, NULL, NULL, &tv);
+}
+
+void handleInput() {
+    if (kbhit()) {
+        char input;
+        read(STDIN_FILENO, &input, 1);
+        switch (input) {
+            case 'a':
+                moveTetromino(-1, 0);
+                break;
+            case 'd':
+                moveTetromino(1, 0);
+                break;
+            case 's':
+                fastDrop = 1; // Acelera a queda
+                break;
+            case 'q':
+                fastDrop = 0; // Restaura a velocidade normal
+                break;
+        }
+    } else {
+        fastDrop = 0; // Restaura a velocidade normal se nenhuma tecla estiver pressionada
+    }
+}
